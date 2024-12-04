968965c3a14539b7f91ea4dc9786f995df21353f
Abdulboriy Isamidinov <101977063+Iasm789@users.noreply.github.com>
2024-12-04 17:20:18 +0300

    Update faq.py

--- bot/handlers/faq.py
+++ bot/handlers/faq.py
@@ -6,13 +6,14 @@ from bot.handlers.faq_data import questions_and_answers
 
 
 faq_router = Router()
+question_list = "\n".join([f"{i + 1}. {question}" for i, question in enumerate(questions_and_answers.keys())])
 
 
 @faq_router.message(F.text == "❓ FAQ")
 @faq_router.message(Command("faq"))
 async def faq_command_handler(message: Message) -> None:
     await message.answer(
-        text="Выберите вопрос из списка, чтобы получить ответ:",
+        text=f"Выберите интересующий вас вопрос:\n\n{question_list}",
         reply_markup=generate_faq_keyboard()
     )
 
@@ -35,6 +36,6 @@ async def answer_faq_callback(callback: CallbackQuery) -> None:
 @faq_router.callback_query(F.data == "back_to_faq")
 async def back_to_faq_handler(callback: CallbackQuery) -> None:
     await callback.message.edit_text(
-        text="Выберите вопрос из списка, чтобы получить ответ:",
+        text=f"Выберите вопрос из списка, чтобы получить ответ:\n\n{question_list}",
         reply_markup=generate_faq_keyboard()
     )
